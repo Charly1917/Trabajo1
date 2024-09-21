1b82fec05330cf95de88cb38af461c299f90481a
Charly1917 <carlitosmar1922@gmail.com>
2024-09-20 20:08:33 -0600

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Primer_Semestre;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Nodo {
+    public String sId="";
+    public boolean bEstado=false;
+    public ArrayList<String> Ruta=null;
+    public ArrayList<Nodo> hijos=null;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
